2111581d6332e4be8e8e07839b6bb213d5e4e684
kadelcode <kadelcode@gmail.com>
2025-12-07 15:50:25 +0100

    test: add more test cases to verify functionality for `reverseArray`

--- src/data-structures/arrays/basics/__tests__/reverseArray.test.ts
+++ src/data-structures/arrays/basics/__tests__/reverseArray.test.ts
@@ -6,4 +6,29 @@ describe('reverseArray', () => {
     const expected = [6, 4, 3, 2, 1];
     expect(reverseArray(input)).toEqual(expected);
   });
+
+  it('should reverse an array with even number of elements', () => {
+    const input = [10, 20, 30, 40];
+    const expected = [40, 30, 20, 10];
+    expect(reverseArray(input)).toEqual(expected);
+  });
+
+  it('should return the same array if it has only one element', () => {
+    const input = [8];
+    const expected = [8];
+    expect(reverseArray(input)).toEqual(expected);
+  });
+
+  it('should return an empty array if the input is empty', () => {
+    const input: number[] = [];
+    const expected: number[] = [];
+    expect(reverseArray(input)).toEqual(expected);
+  });
+
+  it('should modify the original array', () => {
+    const input = [1, 2, 3];
+    const originalInput = [...input];
+    reverseArray(input);
+    expect(input).not.toEqual(originalInput);
+  });
 });
